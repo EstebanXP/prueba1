b4ddac71ff10eef98bea446e8b6eae7c0901df32
Efrain Estrada <a325060@uach.mx>
2021-09-26 15:28:12 -0600

    Cmbiuos

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
 
   return (
     <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
-      <Text style={{fontSize:30}}>Hola mundo</Text>
+      <Text style={{fontSize:30}}>Cualquier otra cosa</Text>
     </View>
   );
 }
