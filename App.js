0056af1cd23f57db07b39ba174dbbc43c2717769
EstebanXP <a324249@uach.mx>
2021-09-26 13:43:49 -0600

    prueba1

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+
+  return (
+    <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
+      <Text style={{fontSize:30}}>Hola mundo</Text>
+    </View>
+  );
+}
+
